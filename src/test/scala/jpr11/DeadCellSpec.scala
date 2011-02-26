0a01702723d7f9269c87d3b6860622af48dced70
Guy Gascoigne-Piggford <guy@peregrine.local>
2011-02-25 17:01:58 -0700

    minor formastting changes

---
+++ src/test/scala/jpr11/DeadCellSpec.scala
@@ -0,0 +1,31 @@
+package jpr11
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+class DeadCellSpec extends FlatSpec with ShouldMatchers {
+
+  "A dead cell with three live neighbors" should "become alive" in {
+    DeadCell.nextGeneration(3) should be(AliveCell)
+  }
+
+  "A dead cell with two live neighbors" should "stay dead" in {
+    DeadCell.nextGeneration(2) should be(DeadCell)
+  }
+
+  "A dead cell with four live neighbors" should "stay dead" in {
+    DeadCell.nextGeneration(4) should be(DeadCell)
+  }
+
+  "A dead cell with minimum possible live neighbors" should "stay dead" in {
+    DeadCell.nextGeneration(Integer.MIN_VALUE) should be(DeadCell)
+  }
+
+  "A dead cell with maximum possible live neighbors" should "stay dead" in {
+    DeadCell.nextGeneration(Integer.MAX_VALUE) should be(DeadCell)
+  }
+
+}
+
+
+// vim: set ts=4 sw=4 et:
