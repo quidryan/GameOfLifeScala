0959d969503876ae584d24480679a59e5149ff79
Ståle Undheim <staale.undheim@ums.no>
2011-03-11 15:09:19 +0100

    Added Idea plugin to generate idea project. Just write sbt idea to generate Intellij project files.

--- project/build/ConwaysGameOfLife.scala
+++ project/build/ConwaysGameOfLife.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class ConwaysGameOfLife(info: ProjectInfo) extends DefaultProject(info) {
+class ConwaysGameOfLife(info: ProjectInfo) extends DefaultProject(info) with IdeaProject {
     val scalaToolsRepository = "Scala-Tools Maven2 Repository" at "http://www.scala-tools.org/repo-releases/"
     val scalatest = "org.scalatest" % "scalatest" % "1.3"
     val scalaswing = "org.scala-lang" % "scala-swing" % "2.8.1"
