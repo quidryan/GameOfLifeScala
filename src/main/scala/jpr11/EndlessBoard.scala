316de1ec404c5e6f91ca2c4255c24a7d0ef0ef99
Ståle Undheim <staale@staale.org>
2011-03-13 16:28:15 +0100

    Removed unused functions. Added constructor taking traversable Delegated evolve behaviour to the Cell class.

--- src/main/scala/jpr11/EndlessBoard.scala
+++ src/main/scala/jpr11/EndlessBoard.scala
@@ -5,17 +5,19 @@
 package jpr11
 
 object EndlessBoard {
+  import util.Random.nextBoolean
+
   def randomBoard(width:Int, height: Int) = {
-    val aliveLocations = for {
-      x <- 0 to width
-      y <- 0 to height
-      if(util.Random.nextBoolean)
-    } yield Location(x, y)
-    new EndlessBoard(aliveLocations.toSet)
+    new EndlessBoard(
+      (0 to width).flatMap((x) => (0 to height).map((y) => Location(x,y)))
+        .filter((_) => nextBoolean))
   }
 }
 
 class EndlessBoard(val aliveCells:Set[Location]) {
+
+  def this(aliveCells:Traversable[Location]) = this(aliveCells.toSet)
+
   val minX = aliveCells.map(_.x).min
   val maxX = aliveCells.map(_.x).max
   val minY = aliveCells.map(_.y).min
@@ -32,23 +34,11 @@ class EndlessBoard(val aliveCells:Set[Location]) {
       .mapValues(_.size)
       // Collect only live cells
       .collect {
-        case (location, 3) => location
-        case (location, 2) if aliveCells(location) => location
-      }.toSet
+        case (location, count) if (apply(location).createNextGeneration(count) == AliveCell) => location
+      }
   )
 
-  val grid = (for {
-      x <- minX to maxX
-      y <- minY to maxY
-      val location = Location(x, y)
-    } yield location -> apply(location)
-  ).toMap
-
-  def apply(location:Location) = if (aliveCells.contains(location)) AliveCell else DeadCell
-
-  def withinBounds(x1: Int, y1:Int, x2:Int, y2:Int) = new EndlessBoard(
-    aliveCells.filter((l) => l.x >= x1 && l.x <= x2 && l.y >= y1 && l.y <= y2)
-  )
+  def apply(location:Location) = if (aliveCells(location)) AliveCell else DeadCell
 
   def getNeighbors(l:Location) =
     Location(l.x-1, l.y-1) :: Location(l.x,   l.y-1) ::  Location(l.x+1, l.y-1) ::
